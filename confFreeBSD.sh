7ac71e1c74837785cb1fac36259e8723444066da
maxihatop <khovayko@gmail.com>
2016-06-12 12:09:48 -0400

    Deleted X509 makefile.FreeBSD bldFreeBSD.sh

--- confFreeBSD.sh
+++ confFreeBSD.sh
@@ -3,5 +3,5 @@ CPPFLAGS='-I/usr/local/include/db48/ -I/usr/local/include'
 export CPPFLAGS
 LDFLAGS='-L/usr/local/lib/db48/ -L/usr/local/lib'
 export LDFLAGS
-./configure
-#./configure --enable-debug
+#./configure
+./configure --enable-debug
