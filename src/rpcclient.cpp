766b10f55163b3e9a15dd0626c19292f5377c4f3
EvgenijM86 <Evgenij1@hotmail.com>
2016-12-07 20:52:55 +0200

    rpc: add hex and base64 input/output to name_show, name_new, name_update

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -100,9 +100,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
 
     // emercoin:
     { "name_new", 2 },
-    { "name_new", 4 },
     { "name_update", 2 },
-    { "name_update", 4 },
     { "name_filter", 1 },
     { "name_filter", 2 },
     { "name_filter", 3 },
