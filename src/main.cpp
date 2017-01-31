1a4d31efd5f17faa9c410f22aeacade0cbecbadb
maxihatop <khovayko@gmail.com>
2017-01-31 00:40:31 -0500

    Added Release() after nodes usage, and iterate vNodesCopy

--- src/main.cpp
+++ src/main.cpp
@@ -4871,8 +4871,12 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             }
 
             pfrom->hashCheckpointKnown = checkpoint.hashCheckpoint;
-            BOOST_FOREACH(CNode* pnode, vNodes)
+            BOOST_FOREACH(CNode* pnode, vNodesCopy)
                 checkpoint.RelayTo(pnode);
+
+            LOCK(cs_vNodes);
+            BOOST_FOREACH(CNode* pnode, vNodesCopy)
+                pnode->Release();
         }
     }
 
