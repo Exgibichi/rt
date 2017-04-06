dd1572c365ef7a5318df193ef87d6e4cf9f29e07
EvgenijM86 <EvgenijM86@yandex.com>
2017-04-06 13:27:40 +0300

    add help message for last commit

--- src/init.cpp
+++ src/init.cpp
@@ -249,9 +249,11 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += "  -?                     " + _("This help message") + "\n";
     strUsage += "  -alertnotify=<cmd>     " + _("Execute command when a relevant alert is received or we see a really long fork (%s in cmd is replaced by message)") + "\n";
     strUsage += "  -alerts                " + strprintf(_("Receive and display P2P network alerts (default: %u)"), DEFAULT_ALERTS);
+    strUsage += "  -alertpubkey           " + _("Set alert public key. 0 - disable, 1 - default key, hex string - custom key");
     strUsage += "  -blocknotify=<cmd>     " + _("Execute command when the best block changes (%s in cmd is replaced by block hash)") + "\n";
     strUsage += "  -checkblocks=<n>       " + strprintf(_("How many blocks to check at startup (default: %u, 0 = all)"), 288) + "\n";
     strUsage += "  -checklevel=<n>        " + strprintf(_("How thorough the block verification of -checkblocks is (0-4, default: %u)"), 3) + "\n";
+    strUsage += "  -checkpointpubkey           " + _("Set checkpoint public key. 0 - disable, 1 - default key, hex string - custom key");
     strUsage += "  -conf=<file>           " + strprintf(_("Specify configuration file (default: %s)"), "emercoin.conf") + "\n";
     if (mode == HMM_BITCOIND)
     {
