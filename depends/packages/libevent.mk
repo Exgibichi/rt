169a6bd6441d539fa5f29257af69b3c40982ac68
EvgenijM86 <EvgenijM86@yandex.com>
2017-08-11 19:00:18 +0300

    fix linux gitian build

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -3,13 +3,14 @@ $(package)_version=2.1.7
 $(package)_download_path=https://github.com/libevent/libevent/archive/
 $(package)_file_name=release-$($(package)_version)-rc.tar.gz
 $(package)_sha256_hash=548362d202e22fe24d4c3fad38287b4f6d683e6c21503341373b89785fa6f991
+$(package)_dependencies=openssl
 
 define $(package)_preprocess_cmds
   ./autogen.sh
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress
+  $(package)_config_opts=--disable-shared --disable-libevent-regress
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
 endef
