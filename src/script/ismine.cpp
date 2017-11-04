b859e1171ce566237b43e0243ac26394e876b62c
EvgenijM86 <EvgenijM86@yandex.com>
2017-11-04 19:02:46 +0200

    fix IsMine function so that it can identify names correctly

--- src/script/ismine.cpp
+++ src/script/ismine.cpp
@@ -46,7 +46,6 @@ isminetype IsMineInner(const CKeyStore &keystore, const CScript& scriptPubKey, b
     case TX_NONSTANDARD:
     case TX_NULL_DATA:
         break;
-    case TX_NAME: fName = true;  // name tx is represented as TX_PUBKEY internaly, that is why we do not "break;" before TX_PUBKEY
     case TX_PUBKEY:
         keyID = CPubKey(vSolutions[0]).GetID();
         if (sigversion != SIGVERSION_BASE && vSolutions[0].size() != 33) {
@@ -69,6 +68,7 @@ isminetype IsMineInner(const CKeyStore &keystore, const CScript& scriptPubKey, b
             return ret;
         break;
     }
+    case TX_NAME: fName = true;  // name tx is represented as TX_PUBKEYHASH internaly, that is why we do not "break;" before TX_PUBKEYHASH
     case TX_PUBKEYHASH:
         keyID = CKeyID(uint160(vSolutions[0]));
         if (sigversion != SIGVERSION_BASE) {
