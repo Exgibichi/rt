eecb15880b51dfbccdc1b322999b77481821eac5
EvgenijM86 <EvgenijM86@yandex.com>
2017-12-08 11:59:10 +0200

    temporarily reduce default tx version, until BIP68 fork

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -352,7 +352,7 @@ class CTransaction
 {
 public:
     // Default transaction version.
-    static const int32_t CURRENT_VERSION=2;
+    static const int32_t CURRENT_VERSION=1;  //emc - change this to 2 after BIP68 fork happens.
 
     // Changing the default transaction version requires a two step process: first
     // adapting relay policy by bumping MAX_STANDARD_VERSION, and then later date
