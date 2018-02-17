691f5d295e2618bfe1ccb8bbdd3a93332d45ae15
EvgenijM86 <EvgenijM86@yandex.com>
2018-02-17 13:02:09 +0200

    fix block encodings for BIP152

--- src/blockencodings.cpp
+++ src/blockencodings.cpp
@@ -19,7 +19,14 @@
 
 CBlockHeaderAndShortTxIDs::CBlockHeaderAndShortTxIDs(const CBlock& block, bool fUseWTXID) :
         nonce(GetRand(std::numeric_limits<uint64_t>::max())),
-        shorttxids(block.vtx.size() - 1), prefilledtxn(1), header(block) {
+        shorttxids(block.vtx.size() - 1), prefilledtxn(1) {
+
+    // emercoin: get header with correct nFlags and auxpow
+    BlockMap::iterator mi = mapBlockIndex.find(block.GetHash());
+    assert(mi != mapBlockIndex.end()); // emercoin: we should always have index for a block that we have accepted
+    header = (*mi).second->GetBlockHeader();
+    vchBlockSig = block.vchBlockSig;
+
     FillShortTxIDSelector();
     //TODO: Use our mempool prior to block acceptance to predictively fill more than just the coinbase
     prefilledtxn[0] = {0, block.vtx[0]};
@@ -55,6 +62,7 @@ ReadStatus PartiallyDownloadedBlock::InitData(const CBlockHeaderAndShortTxIDs& c
 
     assert(header.IsNull() && txn_available.empty());
     header = cmpctblock.header;
+    vchBlockSig = cmpctblock.vchBlockSig;
     txn_available.resize(cmpctblock.BlockTxCount());
 
     int32_t lastprefilledindex = -1;
@@ -179,6 +187,7 @@ ReadStatus PartiallyDownloadedBlock::FillBlock(CBlock& block, const std::vector<
     assert(!header.IsNull());
     uint256 hash = header.GetHash();
     block = header;
+    block.vchBlockSig = vchBlockSig;
     block.vtx.resize(txn_available.size());
 
     size_t tx_missing_offset = 0;
