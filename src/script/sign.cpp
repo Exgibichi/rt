1652af8e1f00df3414dce745e8afcf37d0777708
EvgenijM86 <EvgenijM86@yandex.com>
2018-03-05 23:09:08 +0200

    fix bug in SignStep

--- src/script/sign.cpp
+++ src/script/sign.cpp
@@ -82,6 +82,7 @@ static bool SignStep(const BaseSignatureCreator& creator, const CScript& scriptP
     {
     case TX_NONSTANDARD:
     case TX_NULL_DATA:
+        return false;
     case TX_PUBKEY:
         keyID = CPubKey(vSolutions[0]).GetID();
         return Sign1(keyID, creator, scriptPubKey, ret, sigversion);
